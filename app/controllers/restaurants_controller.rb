aa4995a6b60c3e609683ad65b2754b03060355a1
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-12 11:12:52 -0400

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,19 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    if params[:search]
+      @restaurants = Restaurant.search(params[:search])
+    else
+      @restaurants = Restaurant.all
+    end
   end
+
+  def show 
+  	@restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  	def rest_params
+  	  params.require(:restaurant).permit(:name, :address, :search)
+  	end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,16 @@
 class Restaurant < ApplicationRecord
-  
+  has_many :pizzas
+
+  def self.search(search)
+	if search
+	  restaurant = Restaurant.find_by(name: search)
+	  if restaurant
+	  	[restaurant]
+	  else
+	  	Restaurant.all
+	  end
+	else
+	  Restaurant.all
+	end
+  end
 end
